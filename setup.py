74e5791ecc8de57059cdb5e195dbafd412c5b8a3
tbgitoo <thomas.braschler@gmail.com>
2021-03-08 16:40:17 +0100

    For release v1.0

--- setup.py
+++ setup.py
@@ -28,5 +28,6 @@ setup(name='particleShear',
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
           "Operating System :: OS Independent",
-      ]
+      ],
+    python_requires='>=3.7'
       )
